440d929a1b9bf6dafacb0eee443cb169430ff5a7
zhoumingjun <zhoumingjun@gmail.com>
2018-08-15 11:01:30 +0800

    move content to content directory

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,6 +21,13 @@ module.exports = {
       },
     },
     {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content`,
+        name: 'content',
+      },
+    },
+    {
       resolve: `gatsby-transformer-remark`,
       options: {
         plugins: [
