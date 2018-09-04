a33c24186856c7e7b236dc0e61283337399d117c
zhoumingjun <zhoumingjun@gmail.com>
2018-09-05 02:18:25 +0800

    update

--- src/pages/about.js
+++ src/pages/about.js
@@ -6,9 +6,24 @@ import {Box, Heading, Text} from 'grommet';
 const Index = props => {
   return (
     <Layout>
-      <Box direction="row" basis="xlarge" justify="center">
-        <Box direction="column" pad="xlarge" width="xlarge" align="center">
-          <Text>This is my personal blog for technical things.</Text>
+      <Box direction="column" pad="xlarge" width="xlarge">
+        <Text>This is my personal blog for technical things.</Text>
+
+        <Heading level={3} margin="none">
+          History:
+        </Heading>
+
+        <Box direction="row" justify="start" align="start" gap="medium">
+          <Heading level={4} margin="none">
+            2018-09-05
+          </Heading>
+          <Text>add tag cloud</Text>
+        </Box>
+        <Box direction="row" justify="start" align="start" gap="medium">
+          <Heading level={4} margin="none">
+            2018-08-14
+          </Heading>
+          <Text>build blog with gatsby/grommet</Text>
         </Box>
       </Box>
     </Layout>
