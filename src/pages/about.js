00ae21d1d709fb9f64d1c8acfe7e0ebc2e7bfdae
zhoumingjun <zhoumingjun@gmail.com>
2018-09-05 02:24:38 +0800

    update

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,6 +2,12 @@ import React from 'react';
 import {graphql} from 'gatsby';
 import Layout from '../components/Layout';
 import {Box, Heading, Text} from 'grommet';
+import _ from 'lodash';
+
+const logs = [
+  {date: '2018-09-05', content: 'add tag cloud'},
+  {date: '2018-08-14', content: 'build blog with gatsby/grommet'},
+];
 
 const Index = props => {
   return (
@@ -9,22 +15,23 @@ const Index = props => {
       <Box direction="column" pad="xlarge" width="xlarge">
         <Text>This is my personal blog for technical things.</Text>
 
-        <Heading level={3} margin="none">
-          History:
-        </Heading>
+        <Heading level={3}>History:</Heading>
 
-        <Box direction="row" justify="start" align="start" gap="medium">
-          <Heading level={4} margin="none">
-            2018-09-05
-          </Heading>
-          <Text>add tag cloud</Text>
-        </Box>
-        <Box direction="row" justify="start" align="start" gap="medium">
-          <Heading level={4} margin="none">
-            2018-08-14
-          </Heading>
-          <Text>build blog with gatsby/grommet</Text>
-        </Box>
+        {logs.map((log, key) => {
+          return (
+            <Box
+              key={key}
+              direction="row"
+              justify="start"
+              align="start"
+              gap="medium">
+              <Heading level={4} margin="none">
+                {log.date} :
+              </Heading>
+              <Text>{log.content}</Text>
+            </Box>
+          );
+        })}
       </Box>
     </Layout>
   );
